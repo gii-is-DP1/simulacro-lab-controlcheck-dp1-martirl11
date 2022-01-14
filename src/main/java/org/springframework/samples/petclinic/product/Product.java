ace1ef61cae4f08af60b8b5a0d8264d956270daf
marta <reyeslopezmarta2001@gmail.com>
2022-01-14 12:11:31 +0100

    hasta el test 9 bien

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -2,12 +2,32 @@ package org.springframework.samples.petclinic.product;
 
 import lombok.Getter;
 import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity {
+    
+    @NotNull
+    @Size(min = 3, max = 50)
     String name;
+
+    @NotNull
+    @Min(0)
     double price;
+    
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name = "product_type_id")
     ProductType productType;
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductController.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -1,5 +1,29 @@
 package org.springframework.samples.petclinic.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.convert.ReadingConverter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/product")
 public class ProductController {
     
+    @Autowired
+    ProductService productService;
+
+    private static final String CREATE_PRODUCT = "products/createOrUpdateProductForm";
+
+    @GetMapping(path = "/create")
+    public String createProduct(ModelMap model ){
+        model.addAttribute("product", new Product());
+        model.addAttribute("productTypes", productService.findAllProductTypes());
+        
+        return CREATE_PRODUCT;
+
+    }
+
 }
